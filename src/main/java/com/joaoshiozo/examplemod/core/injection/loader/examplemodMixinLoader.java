a4cf7d1e1888fe1937e45fc6cefb2ac0f88d6bcf
JohnShiozo <joaoshiozo@gmail.com>
2022-01-10 00:23:28 -0300

    push

---
+++ src/main/java/com/joaoshiozo/examplemod/core/injection/loader/examplemodMixinLoader.java
@@ -0,0 +1,43 @@
+package com.joaoshiozo.examplemod.core.injection.loader;
+
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.MixinEnvironment;
+import org.spongepowered.asm.mixin.Mixins;
+
+import java.util.Map;
+
+@IFMLLoadingPlugin.MCVersion(value = "1.7.10")
+public class examplemodMixinLoader implements IFMLLoadingPlugin {
+
+    public examplemodMixinLoader() {
+        MixinBootstrap.init();
+        Mixins.addConfiguration("mixins.examplemod.json");
+        MixinEnvironment.getDefaultEnvironment().setObfuscationContext("searge");
+    }
+
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[0];
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data)
+    {
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+}
---
+++ src/main/java/com/joaoshiozo/examplemod/core/injection/mixins/MixinMinecraft.java
@@ -0,0 +1,21 @@
+package com.joaoshiozo.examplemod.core.injection.mixins;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+
+import org.lwjgl.opengl.Display;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public class MixinMinecraft {
+    public GuiScreen currentScreen;
+
+    @Inject(at = @At("RETURN"), method = "startGame")
+    private void startGame(CallbackInfo info) {
+        Display.setTitle("ExampleMod");
+    }
+}
